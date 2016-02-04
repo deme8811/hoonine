679afc33913ba8aedef1d99af6023c8c8fbb9a2d
shaylasohn <shaylasohn@gmail.com>
2016-02-04 17:29:06 +0900

    123

--- HOONINE/src/hooni/goods/GoodsServiceImpl.java
+++ HOONINE/src/hooni/goods/GoodsServiceImpl.java
@@ -1,10 +1,23 @@
 package hooni.goods;
 
+import java.util.ArrayList;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 @Service
 @Transactional
 public class GoodsServiceImpl implements GoodsService {
+	
+	@Autowired
+	private GoodsDAO dao;
+	
+
+	@Override
+	public ArrayList<Goods> goodsList() {
+		
+		return dao.goodsList();
+	}
 
 }
