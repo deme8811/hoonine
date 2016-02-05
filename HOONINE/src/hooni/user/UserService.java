0cfe82b1d5af99a99c668940077443df4ce78e52
minjeung95 <minjeung95@naver.com>
2016-02-05 15:36:55 +0900

    vbn

--- HOONINE/src/hooni/user/UserService.java
+++ HOONINE/src/hooni/user/UserService.java
@@ -1,5 +1,17 @@
 package hooni.user;
 
+import java.util.ArrayList;
+
 public interface UserService {
+
+	int userJoin(User user);
+
+	User userDetail(String userId);
+
+	User userLogin(User user);
+
+	User userIdCheck(User user);
+
+	ArrayList<User> userList();
 
 }
